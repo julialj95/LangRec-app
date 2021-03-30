39c9e32cae28a8afe85b823ae8ef4e8baad06af1
Julia Johnson <julialj95@gmail.com>
2021-03-29 21:11:58 -0500

    added recommendations fetch

--- src/RecsPage.js
+++ src/RecsPage.js
@@ -1,79 +1,142 @@
-import React from "react";
+import React, { Component } from "react";
+import config from "./config";
 
-function RecsPage() {
-  return (
-    <div>
-      <h2>Get Reccomendations</h2>
-      <h3>
-        Enter your information to receive the highest rated resources that fit
-        your needs!
-      </h3>
-      <form>
-        <label htmlFor="language">
-          Language
-          <select name="language">
-            <option value={null}>Select language...</option>
-            <option value="Spanish">Spanish</option>
-            <option value="French">French</option>
-            <option value="German">German</option>
-            <option value="Korean">Korean</option>
-            <option value="Mandarin">Mandarin</option>
-            <option value="Italian">Italian</option>
-            <option value="Cantonese">Cantonese</option>
-            <option value="Japanese">Japanese</option>
-            <option value="Swahili">Swahili</option>
-            <option value="Afrikaans">Afrikaans</option>
-            <option value="other">Other</option>
-          </select>
-        </label>
-        <br />
-        <label htmlFor="type">
-          Resource Type
-          <select name="type">
-            <option value={null}>Select Type...</option>
-            <option value="textbook">Textbook</option>
-            <option value="storybook">Storybook</option>
-            <option value="class">Class</option>
-            <option value="website">Website</option>
-            <option value="other">Other</option>
-          </select>
-        </label>
-        <br />
-        <br />
-        <label>
-          Level
-          <br />
-          <input type="radio" id="beginner" name="level" value="beginner" />
-          <label for="beginner">Beginner</label>
-          <br />
-          <input
-            type="radio"
-            id="intermediate"
-            name="level"
-            value="intermediate"
-          />
-          <label for="intermediate">Intermediate</label>
-          <br />
-          <input type="radio" id="advanced" name="level" value="advanced" />
-          <label for="advanced">Advanced</label>
-        </label>
-        <br />
-        <br />
-        <label>
-          Cost
-          <br />
-          <input type="radio" name="cost" value="Free" />
-          <label htmlFor="cost">Free</label>
-          <br />
-          <input type="radio" name="cost" value="Paid" />
-          <label htmlFor="cost">Paid</label>
-          <br />
-        </label>
+class RecsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      language: "",
+      type: "",
+      level: "",
+      cost: "",
+    };
+  }
 
-        <button type="submit">Get Recs!</button>
-      </form>
-    </div>
-  );
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  getRecommendations = () => {
+    const { language, type, level, cost } = this.state;
+    const baseUrl = config.API_ENDPOINT;
+    const params = `?language=${language}&type=${type}&level=${level}&cost=${cost}`;
+    fetch(`${baseUrl}/recs/${params}`, {
+      method: GET,
+      headers: {
+        "content-type": "application/json",
+        Authorization: "Bearer " + config.API_KEY,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((event) => Promise.reject(event));
+        }
+        return response.json();
+      })
+      .catch((error) => console.error({ error }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Get Reccomendations</h2>
+        <h3>
+          Enter your information to receive the highest rated resources that fit
+          your needs!
+        </h3>
+        <form onSubmit={this.getRecommendations}>
+          <label htmlFor="language">
+            Language
+            <select name="language" onChange={(e) => this.handleChange(e)}>
+              <option value={null}>Select language...</option>
+              <option value="Spanish">Spanish</option>
+              <option value="French">French</option>
+              <option value="German">German</option>
+              <option value="Korean">Korean</option>
+              <option value="Mandarin">Mandarin</option>
+              <option value="Italian">Italian</option>
+              <option value="Cantonese">Cantonese</option>
+              <option value="Japanese">Japanese</option>
+              <option value="Swahili">Swahili</option>
+              <option value="Afrikaans">Afrikaans</option>
+              <option value="other">Other</option>
+            </select>
+          </label>
+          <br />
+          <label htmlFor="type">
+            Resource Type
+            <select name="type" onChange={(e) => this.handleChange(e)}>
+              <option value={null}>Select Type...</option>
+              <option value="textbook">Textbook</option>
+              <option value="storybook">Storybook</option>
+              <option value="class">Class</option>
+              <option value="website">Website</option>
+              <option value="other">Other</option>
+            </select>
+          </label>
+          <br />
+          <br />
+          <label>
+            Level
+            <br />
+            <input
+              type="radio"
+              id="beginner"
+              name="level"
+              value="beginner"
+              onChange={(e) => this.handleChange(e)}
+            />
+            <label htmlFor="beginner">Beginner</label>
+            <br />
+            <input
+              type="radio"
+              id="intermediate"
+              name="level"
+              value="intermediate"
+              onChange={(e) => this.handleChange(e)}
+            />
+            <label htmlFor="intermediate">Intermediate</label>
+            <br />
+            <input
+              type="radio"
+              id="advanced"
+              name="level"
+              value="advanced"
+              onChange={(e) => this.handleChange(e)}
+            />
+            <label htmlFor="advanced">Advanced</label>
+          </label>
+          <br />
+          <br />
+          <label>
+            Cost
+            <br />
+            <input
+              type="radio"
+              name="cost"
+              value="Free"
+              onChange={(e) => this.handleChange(e)}
+            />
+            <label htmlFor="cost">Free</label>
+            <br />
+            <input
+              type="radio"
+              name="cost"
+              value="Paid"
+              onChange={(e) => this.handleChange(e)}
+            />
+            <label htmlFor="cost">Paid</label>
+            <br />
+          </label>
+
+          <button type="submit">Get Recs!</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default RecsPage;
